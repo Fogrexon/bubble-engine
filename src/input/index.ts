22701f49d7bd6051bfa63369f9ef3bc53fcc4c77
fogrex <fogrexon@gmail.com>
2023-07-21 14:59:36 +0900

    move inputmanager

--- src/input/index.ts
+++ src/input/index.ts
@@ -1,45 +1,6 @@
-import {InputableKey, inputableKeyList, InputKeybind, RawKeyValue} from './InputDefinition';
-import {KeyBinder} from './KeyBinder';
-
-export class InputManager<T extends Record<string, InputKeybind>> {
-  private keybinds: T;
-
-  private keybinders: { [K in keyof T]: KeyBinder<T[K]> };
-
-  private rawKeyValues: RawKeyValue;
-
-  constructor(window: Window, keybinds: T) {
-    this.keybinds = keybinds;
-    this.keybinders = Object.keys(keybinds).reduce((acc, key: keyof T) => {
-      acc[key] = new KeyBinder(keybinds[key]);
-      return acc;
-    }, {} as { [K in keyof T]: KeyBinder<T[K]> });
-    this.rawKeyValues = inputableKeyList.reduce((acc, key) => {
-      acc[key] = 0;
-      return acc;
-    }, {} as RawKeyValue) as RawKeyValue;
-
-    window.addEventListener('keydown', (e) => {
-      this.rawKeyValues[e.key as InputableKey] = 1;
-    });
-    window.addEventListener('keyup', (e) => {
-      this.rawKeyValues[e.key as InputableKey] = 0;
-    });
-
-  }
-
-  public getKeybinder<Key extends keyof typeof this.keybinders>(key: Key): typeof this.keybinders[Key] {
-    return this.keybinders[key];
-  }
-
-  public updateKeyBinds() {
-    Object.keys(this.keybinds).forEach((key) => {
-      this.keybinders[key].update(this.rawKeyValues);
-    });
-  }
-
-  // イベントハンドラ系
-  private keydownHandler(e: KeyboardEvent) {
-    this.rawKeyValues[e.key as InputableKey] = 1;
-  }
-}
+export * from './InputManager'
+export * from './IInputStateProvidable'
+export * from './InputDefinition'
+export * from './KeyBinder'
+export * from './ScalarProvider'
+export * from './Vector2Provider'
